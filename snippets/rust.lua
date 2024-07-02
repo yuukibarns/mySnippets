11f6f1348e9ad33f4d2ec67a938b7c0ac8d95dcc
yuukibarns <snrabikuuy@sina.com>
2024-07-02 19:30:37 +0800

    fix: rust.lua

--- snippets/rust.lua
+++ snippets/rust.lua
@@ -5,7 +5,7 @@ snips = {
 		trig = "io",
 		name = "io.read_line",
 		desc = "io::stdin().read_line(&mut :buf)",
-	}),
+	}, fmta([[io::stdin().read_line(&mut <>)<>]], { i(1), i(0) })),
 }
 
 return snips
