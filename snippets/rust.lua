c31096deff67395e5290ebafec4dfea7d357f9d9
yuukibarns <snrabikuuy@sina.com>
2024-07-02 23:41:54 +0800

    feat: add a snippet to rust.lua!

--- snippets/rust.lua
+++ snippets/rust.lua
@@ -3,9 +3,15 @@ local snips = {}
 snips = {
 	s({
 		trig = "io",
-		name = "io.read_line",
-		desc = "io::stdin().read_line(&mut :buf)",
-	}, fmta([[io::stdin().read_line(&mut <>)<>]], { i(1), i(0) })),
+		name = "read_line",
+		desc = "io::stdin().read_line(&mut <>).expect();",
+	}, fmta([[io::stdin().read_line(&mut <>).expect("Failed to read line!");]], { i(1), i(0) })),
+
+	s({
+		trig = "tr",
+		name = "transform data type",
+		desc = "let <>: <> = <>.trim().parse().expect(<>);",
+	}, fmta([[let <>: <> = <>.trim().parse().expect("<>");]], { i(1), i(2), i(3), i(0, "Not a number!") })),
 }
 
 return snips
