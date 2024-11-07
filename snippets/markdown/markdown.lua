5d884d39d3fbde2fd43c9d1a4ca9d0df707107d0
yuukibarns <yuukibarns@gmail.com>
2024-11-07 11:01:55 +0800

    Add link snippet

--- snippets/markdown/markdown.lua
+++ snippets/markdown/markdown.lua
@@ -13,14 +13,9 @@ snips = {
 		fmt([[![{}]({} "{}")]], { i(1, "Figure"), i(2, "url"), i(3, "title") }),
 		opts
 	),
-	s(
-		{ trig = "fig", name = "Markdown figure", desc = "Add an image." },
-		fmt([[![{}]({} "{}")]], { i(1, "Figure"), i(2, "url"), i(3, "title") }),
-		opts
-	),
 	s({ trig = "bold", name = "Bold text", desc = "Bold text" }, fmt([[**{}**{}]], { i(1), i(0) }), opts2),
 	s({ trig = "italic", name = "Italic text", desc = "Italic text" }, fmt([[_{}_{}]], { i(1), i(0) }), opts2),
-	-- s({ trig = "link", name = "Link text", desc = "Link text" }, fmt("[{}]", { i(0) }), opts2),
+	s({ trig = "link", name = "Link text", desc = "Link text" }, fmt("[{}]({}{})", { i(1), i(2, "$VAULT_DIR"), i(0) }), opts2),
 	s({ trig = "code", name = "Code text", desc = "Code text" }, fmt([[`{}`{}]], { i(1), i(0) }), opts2),
 	s(
 		{ trig = "figure", name = "Markdown figure with caption", desc = "Add an image with caption" },
